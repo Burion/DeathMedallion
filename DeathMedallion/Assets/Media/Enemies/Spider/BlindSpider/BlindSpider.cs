42f136deea87b9b8283904e0d6a4069877511e9b
Vlad Buryak <vladislavburyak00@gmail.com>
2019-03-01 00:56:15 +0200

    [fix] setup gitignore and del nested repo

---
+++ DeathMedallion/Assets/Media/Enemies/Spider/BlindSpider/BlindSpider.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlindSpider : Enemy
+{
+    
+
+    private void Start()
+    {
+        base.Start();
+        anim = GetComponentInChildren<Animator>();
+        base.Start();
+        Health = 4;
+    }
+    private void Update()
+    {
+        UpdateAnimator();
+    }
+    void UpdateAnimator()
+    {
+        if (mng.Player.transform.position.x < transform.position.x)
+        {
+            anim.SetBool("isLeft", true);
+        }
+        else
+            anim.SetBool("isLeft", false);
+    }
+}
