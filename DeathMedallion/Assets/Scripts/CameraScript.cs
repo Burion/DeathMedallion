17d566c341c879801eb43a8eca166d79b452b2c7
Vlad Buryak <vladislavburyak00@gmail.com>
2019-03-05 13:44:42 +0200

    Added camera behaviour

--- DeathMedallion/Assets/Scripts/CameraScript.cs
+++ DeathMedallion/Assets/Scripts/CameraScript.cs
@@ -6,14 +6,77 @@ public class CameraScript : MonoBehaviour
 {
 
     GameObject Player;
+    bool attached;
+    public float sighRange;
+    Vector3 target;
+    float speed = 10f;
+    bool reached;
     void Start()
     {
+        reached = true;
+        attached = true;
         Player = GameObject.Find("Hero");
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        transform.position = new Vector3(Player.transform.position.x, Player.transform.position.y, transform.position.z);
+        if (attached)
+        {
+            target = new Vector3(Player.transform.position.x, Player.transform.position.y, transform.position.z);
+            if (Vector2.Distance(Player.transform.position, transform.position) > 0.1f && !reached)
+            {
+                Slide();
+            }
+            else
+            {
+                transform.position = target;
+                reached = true;
+            }
+        }
+
+        else
+        {
+            Slide();
+        }
+
+        if (Input.GetAxisRaw("SighVertical") != 0 || Input.GetAxisRaw("SighHorizontal") != 0)
+        {
+            Debug.Log(Input.GetAxisRaw("SighVertical"));
+            Debug.Log(Input.GetAxisRaw("SighHorizontal"));
+            attached = false;
+            LookAround();
+            reached = false;
+        }
+        else
+            attached = true;
+ 
+        
+    }
+    void LookAround()
+    {
+        if(Input.GetAxisRaw("SighVertical") == -1)
+        {
+            target = new Vector3(Player.transform.position.x, Player.transform.position.y + sighRange, transform.position.z);
+        }
+        if (Input.GetAxisRaw("SighVertical") == 1)
+        {
+            target = new Vector3(Player.transform.position.x, Player.transform.position.y - sighRange, transform.position.z);
+        }
+        if (Input.GetAxisRaw("SighHorizontal") == 1)
+        {
+            target = new Vector3(Player.transform.position.x + sighRange, Player.transform.position.y, transform.position.z);
+        }
+        if (Input.GetAxisRaw("SighHorizontal") == -1)
+        {
+            target = new Vector3(Player.transform.position.x - sighRange, Player.transform.position.y, transform.position.z);
+        }
+
+    }
+    void Slide()
+    {
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.Lerp(transform.position, target, step);
+        
     }
 }
